57219ae55f16d3d59138bedabff143a804315df4
Mikhail <mikekarpenko@gmail.com>
2018-11-14 15:51:11 +0200

    work by first mainPage. HTML CSS Code

--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,16 @@
 
 // @include('detect.js')
 // @include('globals.js')
+
+/* **
+* Slick Slider
+*
+** */
+
+$('.fade').slick({
+	dots: true,
+	infinite: true,
+	speed: 500,
+	fade: true,
+	cssEase: 'linear'
+});
--- src/js/vendor.js
+++ src/js/vendor.js
@@ -1,4 +1,4 @@
 // @include('../../node_modules/babel-polyfill/dist/polyfill.js')
 // @include('../../node_modules/bowser/src/bowser.js')
 // @include('../../node_modules/svg4everybody/dist/svg4everybody.js')
-// @include('../../node_modules/jquery/dist/jquery.js')
+//@include('../../node_modules/jquery/dist/jquery.js')
